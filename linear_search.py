1cfdea1f2d7742073143c03b57a697e0f5c6ace7
calebadavis114 <caleb.a.davis114@gmail.com>
2023-10-09 10:25:49 -0700

    Completed

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,18 @@
 def linear_search(value_to_find, array_to_search_through):
-    # your code here
-    pass
+   num = 0
+   for item in array_to_search_through:
+       if(value_to_find == item):
+        return num
+       num += 1
+    
+print(linear_search(1, [2,3,4,5,6,0,7,8]))
 
 def linear_search_global(value_to_find, array_to_search_through):
-    # your code here
-    pass
+    arr = []
+    num = 0
+    for item in array_to_search_through:
+       if(value_to_find == item):
+        arr.append(num)
+       num += 1
+    return arr
+    
